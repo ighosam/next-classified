276c0145b3a3cb0c7152290a354afee20fc53ef6
Sam <darosamy@gmail.com>
2024-01-28 05:00:51 -0500

    create dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Welcome to the dashboard
+   <p>List of to do's</p>
+   <ul>
+    <li>create a good looking dashboard</li>
+    <li>user's profile </li>
+    <li>user able to modified profile info</li>
+    <li>user able to view owned listing and edith</li>
+   </ul>
+
+    </div>
+  )
+}
+
+export default page
